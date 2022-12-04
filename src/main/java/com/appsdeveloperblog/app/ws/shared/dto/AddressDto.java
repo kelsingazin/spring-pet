65f733f139a50cda894458c216913d2e98612194
kelsingazin <kelsingaziny@gmail.com>
2022-12-05 00:29:35 +0600

    FEA: Created AddressDto

---
+++ src/main/java/com/appsdeveloperblog/app/ws/shared/dto/AddressDto.java
@@ -0,0 +1,13 @@
+package com.appsdeveloperblog.app.ws.shared.dto;
+
+public class AddressDto {
+
+    private static final long serialVersionUID = 6835191090898364280L;
+    private long id;
+    private String city;
+    private String country;
+    private String streetName;
+    private String postalCode;
+    private String type;
+    private UserDto userDetails;
+}
