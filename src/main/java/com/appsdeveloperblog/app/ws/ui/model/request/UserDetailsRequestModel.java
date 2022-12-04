5c9428124a0698da51d0e88e7bbe7f0aecd8f425
kelsingazin <kelsingaziny@gmail.com>
2022-12-05 00:26:44 +0600

    FEA: Created AddressRequestModel

--- src/main/java/com/appsdeveloperblog/app/ws/ui/model/request/UserDetailsRequestModel.java
+++ src/main/java/com/appsdeveloperblog/app/ws/ui/model/request/UserDetailsRequestModel.java
@@ -2,10 +2,13 @@ package com.appsdeveloperblog.app.ws.ui.model.request;
 
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 public class UserDetailsRequestModel {
     private String firstName;
     private String lastName;
     private String password;
     private String email;
+    private List<AddressRequestModel> addresses;
 }
